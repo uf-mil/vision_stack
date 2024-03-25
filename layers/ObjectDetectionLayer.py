cf0f46e2c7e54112bb4029e3b8d438e8d65f5c03
Daniel Parra <dparra1@ufl.edu>
2024-03-25 16:03:43 -0400

    Fixed issues with object detection layer

--- layers/ObjectDetectionLayer.py
+++ layers/ObjectDetectionLayer.py
@@ -2,36 +2,60 @@ import cv2
 
 import os
 
+import sys
+sys.path.append(os.getcwd())
+sys.path.append(sys.path[-1]+"/ml/yolov7")
+
 import numpy as np
 import torch
 import torch.nn as nn
 import tensorflow as tf
 from PIL import Image
 from torchvision import transforms
-from ..ml.yolov7.models.experimental import attempt_load
-from ..ml.yolov7.utils.plots import plot_one_box
-from ..ml.yolov7.utils.general import non_max_suppression
+from ml.yolov7.models.experimental import attempt_load
+from ml.yolov7.utils.plots import plot_one_box
+from ml.yolov7.utils.general import non_max_suppression
 
 from Layer import AnalysisLayer
 
 class ObjectDetectionLayer(AnalysisLayer):
-    def __init__(self, in_size, out_size, path_to_weights, conf_thres, iou_thres, class_names_array, colors_array) -> None:
+    def __init__(self, in_size, out_size, path_to_weights, conf_thres, iou_thres, class_names_array = [], colors_array = [], pass_post_processing_img = False) -> None:
         # TODO: determine what type of processor to use based on the .type of the path_to_weights string
-
-        # TODO: set the conf and iou threshold values
+        _, file_type = os.path.splitext(path_to_weights)
+        self.weights_name = os.path.basename(path_to_weights)
 
         # TODO: set the colors array and class_names_array values
+        if len(colors_array) != len(class_names_array):
+            raise Exception(f"ERROR -> ObjectDetectionLayer -> __init__: colors_array must be same length as class_names_array ({len(colors_array)} != {len(class_names_array)})")
 
-        # TODO: set the processor
-        super().__init__(in_size, out_size, "objectDetection")
+        self.class_names_array = class_names_array
+        self.colors_array = colors_array
+
+        self.processor = self.PTWeightsProcessor(path_to_weights, conf_thres, iou_thres, class_names_array, colors_array, pass_post_processing_img) if file_type == ".pt" else self.TFLiteWeightsProcessor(path_to_weights, conf_thres, class_names_array, colors_array, pass_post_processing_img) if file_type == ".tflite" else None
+
+        if not self.processor:
+            raise Exception("ERROR -> ObjectDetectionLayer -> __init__: Invalid file type, weights file supported are *.pt or *.tflite") 
+
+        # TODO: set the conf and iou threshold values
+        self.conf_thres = conf_thres
+        self.iou_thres = iou_thres
+
+        super().__init__(in_size, out_size, f"objectDetection-{self.weights_name}")
+    
+    def process(self, image):
+        return self.processor.process(image)
     
     class PTWeightsProcessor():
-        def __init__(self, weights_path, conf_thres, iou_thres) -> None:
+        def __init__(self, weights_path, conf_thres, iou_thres, classes, colors, pass_post_processing_img = False) -> None:
             self.weights_path = weights_path
+            self.pass_post_processing_img = pass_post_processing_img
             self.device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
             self.conf_thres = conf_thres
             self.iou_thres = iou_thres
+
+            self.colors = colors
+            self.classes = classes
 
             absolute_file_path = os.path.abspath(
                 os.path.join(os.path.dirname(__file__), self.weights_path),
@@ -44,10 +68,14 @@ class ObjectDetectionLayer(AnalysisLayer):
 
         def process(self, img):
             image = Image.fromarray(img)
+            image = image.resize((960, 608))
             img_transform = transforms.Compose([transforms.ToTensor()])
             img_tensor = img_transform(image).to(self.device).unsqueeze(0)
 
+            print(img_tensor.size())
+
             pred_results = self.__MODEL(img_tensor)[0]
+
             detections = non_max_suppression(
                 pred_results,
                 conf_thres=self.conf_thres,
@@ -60,25 +88,29 @@ class ObjectDetectionLayer(AnalysisLayer):
                 detections = detections[0]
                 for x1, y1, x2, y2, conf, cls in detections:
                     class_index = int(cls.cpu().item())
-                    print(f"{self.__CLASSES[class_index]} => {conf}")
+                    print(f"{self.classes[class_index]} => {conf}")
                     plot_one_box(
                         [x1, y1, x2, y2],
                         arr_image,
-                        label=f"{self.__CLASSES[class_index]}",
-                        color=self.__COLORS[class_index],
+                        label=f"{self.classes[class_index]}",
+                        color=self.colors[class_index],
                         line_thickness=2,
                     )
 
-            return arr_image
+            return (arr_image if self.pass_post_processing_img else img, detections)
     
     class TFLiteWeightsProcessor():
-        def __init__(self, weights_path) -> None:
+        def __init__(self, weights_path, conf_thres, classes, colors, pass_post_processing_img = False) -> None:
             self.weights_path = weights_path
+            self.conf_thres = conf_thres
+            self.classes = classes
+            self.colors = colors
+            self.pass_post_processing_img = pass_post_processing_img
 
         def process(self, img):
-            COLORS = [(255,0,0),(0,255,0),(0,0,255),(255,155,0),(255,0,255),(0,255,255)]
-            CLASSES = ['buoy_abydos_serpenscaput', 'buoy_abydos_taurus', 'buoy_earth_auriga', 'buoy_earth_cetus', 'gate_abydos', 'gate_earth']
-            CONFIDENCE_THRESHOLD = 0.77
+            COLORS = self.colors
+            CLASSES = self.classes
+            CONFIDENCE_THRESHOLD = self.conf_thres
 
             # Load the TFLite model and allocate tensors
             interpreter = tf.lite.Interpreter(model_path=self.weights_path)
@@ -132,8 +164,7 @@ class ObjectDetectionLayer(AnalysisLayer):
             else:
                 print("No Detections Made")
 
-            #TODO: Return post processing image or preprocessed image and the detections Center width and height
-            return arr_image
+            return (arr_image if self.pass_post_processing_img else img, detections[0])
         
         def __bounding_box_coordinates(center_x, center_y, width, height):
                 half_width = width / 2
--- layers/VisionStack.py
+++ layers/VisionStack.py
@@ -1,10 +1,11 @@
-import ResizeLayer, GaussianLayer, GrayscaleLayer, BinThresholdingLayer, HoughTransformLayer, RGBMagnificationLayer, UnderwaterEnhancementLayer, CustomLayer
+import ResizeLayer, GaussianLayer, GrayscaleLayer, BinThresholdingLayer, HoughTransformLayer, RGBMagnificationLayer, UnderwaterEnhancementLayer, CustomLayer, ObjectDetectionLayer
 from Layer import Layer
 from typing import List, Tuple
 from datetime import datetime
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
+plt.switch_backend('TkAgg')
 import os
 
 NUM_COLS = 3
@@ -79,7 +80,23 @@ if __name__ == "__main__":
     SIZE = (900,600)
     stack = VisionStack([ResizeLayer.ResizeLayer((0,0), 900, 400), 
                          GaussianLayer.GaussianLayer(SIZE, (5,5), 10)], SIZE)
-    img = Image.open(os.path.join(os.path.dirname(__file__), '../imgs/<YOUR_IMAGE_FILE>'))
+    img = Image.open(os.path.join(os.path.dirname(__file__), '../imgs/original.jpg'))
+    CLASSES = [
+        "buoy_abydos_serpenscaput",
+        "buoy_abydos_taurus",
+        "buoy_earth_auriga",
+        "buoy_earth_cetus",
+        "gate_abydos",
+        "gate_earth",
+    ]
+    COLORS = [
+        (255, 0, 0),
+        (0, 255, 0),
+        (0, 0, 255),
+        (255, 155, 0),
+        (255, 0, 255),
+        (0, 255, 255),
+    ]
     # img.show()
     # stack.visualize()
     def funcToMyLayer(img, args):
@@ -91,6 +108,7 @@ if __name__ == "__main__":
     stack.push(BinThresholdingLayer.BinThresholdingLayer(SIZE, 150, 255))
     stack.push(HoughTransformLayer.HoughTransformLayer(SIZE, 100, 20, 10, True))
     stack.push(GaussianLayer.GaussianLayer(SIZE, (5,5), 15))
+    stack.push(ObjectDetectionLayer.ObjectDetectionLayer(SIZE,SIZE, "../ml/weights/robosub24.pt", 0.5, 0.5, CLASSES, COLORS, True))
     # print()
     # stack.visualize()
     stack.run(np.array(img), True)
