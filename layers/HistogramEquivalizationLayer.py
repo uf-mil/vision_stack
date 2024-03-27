b3345821ebd58c484e88d7e600a6b5a49eb67dc3
AlexanderWangY <alexander.yisu.wang@outlook.com>
2024-03-27 11:34:17 -0400

    finished histogram equlization layers + cmap for grayscale

---
+++ layers/HistogramEquivalizationLayer.py
@@ -0,0 +1,25 @@
+import cv2
+from Layer import PreprocessLayer
+
+class HistogramEqualizaionLayer(PreprocessLayer):
+
+    def __init__(self, size) -> None:
+        """
+        Equalizes the pixels between pixel intesities based on the pixel histogram
+        """
+        super().__init__(size, "histogram-equalization")
+
+
+    def process(self, image):
+        return (cv2.equalizeHist(image), None)
+
+class HistogramAdaptiveEqualizationLayer(PreprocessLayer):
+    def __init__(self, size) -> None:
+        """
+        Adaptively equalizes the pixel histogram
+        """
+        super().__init__(size, "adaptive-histogram-equalization")
+
+    def process(self, image):
+        clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+        return (clahe.apply(image), None)
