db8effb51f182c8cf52b1d33648dfcad6f314c09
WithanagePerera <prav.perera@gmail.com>
2025-10-21 09:09:06 -0400

    Experimenting with incorporating all layers. Running into an issue with the example construction of ObjectDetectionLayer.

--- test/vision_stack_test.py
+++ test/vision_stack_test.py
@@ -18,8 +18,7 @@ sys.path.append(str(Path(__file__).parent.parent))
 #     Image_Subscriber,
 # )
 
-
-from vision_stack import ResizeLayer, UnderWaterImageEnhancementLayer, VisionStack
+from vision_stack import VisionStack, BinThresholdingLayer, CannyLayer, ColorMagnificationLayer, CustomLayer, GaussianLayer, GrayscaleLayer, HoughTransformLayer, MinMaxNormalizationLayer, ZScoreNormalizationLayer, RobustScalingLayer, ObjectDetectionLayer, ResizeLayer, RGBMagnificationLayer, SobelLayer, UnderWaterImageEnhancementLayer
 
 bridge = CvBridge()
 
@@ -37,8 +36,22 @@ class ObjectDetectionTest(Node):
         # camera = rospy.get_param("~image_topic", "/front_cam/image_raw")
         self.vs = VisionStack(
             layers=[
-                ResizeLayer(960, 608),
-                UnderWaterImageEnhancementLayer(),
+                BinThresholdingLayer(150,250), # Converts image to grayscale if image is not grayscale and extracts pixels with values between 150 and 250.
+                CannyLayer(50,100), # Simplified canny filter that uses cv2.Canny to pass a canny filter over an image with the low value (50) threshold for soft edge detection and the high value (100) for strong edges detection.
+                ColorMagnificationLayer((23,156,234)), # Highlights objects with this color (23,156,234) in an image.
+                GaussianLayer((11,11), 50), # Pass a gaussian filter of kernal size (11,11) (kernal size must be of odd numbered dimensions) with a sigma value of 50.
+                GrayscaleLayer(), # Convert image to grayscale
+                HoughTransformLayer(threshold=100, min_line_length=20, max_line_gap=10, pass_post_processing_img=True), # Pass a Hough Transform filter over an image to extract lines. Setting pass_post_processing_img will push the image with hough transform lines to the next layer.
+                # Different types of normalization layers:
+                MinMaxNormalizationLayer(),
+                ZScoreNormalizationLayer(),
+                RobustScalingLayer(),
+                ObjectDetectionLayer('path/to/weights.pt|tflite', conf_thres=0.5, iou_thres=0.5, class_names_array=['cls1','cls2','cls3',...], colors_array=[(255,0,0),(0,255,0),(0,0,255),...], pass_post_processing_img = False), # Access YOLO weights and make predictions on the image provided by the previous layer. Setting pass_post_processing_img will push the image with bounding boxes to the next layer.
+                ResizeLayer(960, 608), # Resize the image from the previous layer.
+                RGBMagnificationLayer('R'|'G'|'B'), # Magnifies the provided channel respectively.
+                SobelLayer((5,5)), # Passes a sobel edge detection layer with a kernal size of (5,5) over the image.
+                UnderWaterImageEnhancementLayer(), # Uses a generative AI model to improve underwater images (good for murky waters).
+                # Include as many layers in any combination as you need
             ],
         )
 
